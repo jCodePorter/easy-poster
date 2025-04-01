03767a9d2fbd5c6e2518918157317b670b326c93
biaoy <jcodeporter@gmail.com>
2025-04-01 18:20:43 +0800

    feat:文本基线调整

--- src/main/java/com/augrain/easy/canvas/model/BaseLine.java
+++ src/main/java/com/augrain/easy/canvas/model/BaseLine.java
@@ -12,9 +12,9 @@ import java.util.function.BiFunction;
 public enum BaseLine {
     BASE_LINE("基线", (fm, lineHeight) -> fm.getHeight() / 2 + (lineHeight - fm.getHeight()) / 2),
 
-    TOP("顶部（即ascent线的位置）", (fm, lineHeight) -> fm.getAscent() + fm.getHeight() / 2 + (lineHeight - fm.getHeight()) / 2),
+    TOP("顶部（即ascent线的位置）", (fm, lineHeight) -> fm.getHeight() / 2 + fm.getAscent() + (lineHeight - fm.getHeight()) / 2),
 
-    BOTTOM("底部（即descent线的位置）", (fm, lineHeight) -> -fm.getDescent() + fm.getHeight() / 2 + (lineHeight - fm.getHeight()) / 2),
+    BOTTOM("底部（即descent线的位置）", (fm, lineHeight) -> fm.getHeight() / 2 - (fm.getDescent() + fm.getLeading()) + (lineHeight - fm.getHeight()) / 2),
 
     CENTER("中心（即centerY线的位置）", (fm, lineHeight) -> fm.getAscent() + (lineHeight - fm.getHeight()) / 2);
 
